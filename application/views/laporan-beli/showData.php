d94e7e891c8ab4c94742e0731e29f9597b500b4e
Akhyar <Nur>
2020-08-13 14:23:25 +0700

    Auto stash before merge of "master" and "origin/master"

--- application/views/laporan-beli/showData.php
+++ application/views/laporan-beli/showData.php
@@ -11,50 +11,12 @@
         </div>
         <?php }?>
         <div class="col-md-12">
-          <div class="box"><div class="box-header">
-                <div class="row">
-                <form action="" method="post">
-                  <div class="col-md-3">
-                      <div class="form-group">
-                          <label>Mulai Tanggal</label>
-                          <input type="date" class="form-control" name="min" placeholder="Kode Obat" value="<?php echo $min ?>" required>
-                      </div>  
-                      
-                  </div>
-                  <!-- /.col -->
-                  <div class="col-md-3">
-                      <div class="form-group">
-                          <label>Sampai Tanggal</label>
-                          <input type="date" class="form-control" name="max" placeholder="Nama Obat" value="<?php echo $max ?>" required>
-                      </div> 
-                      <!-- /.form-group -->
-                  </div>
-                  <div class="col-md-3">
-                      <div class="form-group">
-                          <label>Obat</label>
-                          <input type="text" class="form-control" name="max" placeholder="Nama Obat" value="<?php echo $max ?>" required>
-                      </div> 
-                      <!-- /.form-group -->
-                  </div>
-                  <div class="col-md-3">
-                      <div class="form-group">
-                          <label>Supplier</label>
-                          <input type="text" class="form-control" name="max" placeholder="Nama Obat" value="<?php echo $max ?>" required>
-                      </div> 
-                      <!-- /.form-group -->
-                  </div>
-                  <div class="col-md-1">
-                      <div class="form-group">
-                          <label>&nbsp;</label>
-                          <button type="submit" class="form-control btn btn-success" name="simpan"><i class="fa fa-search"></i></button>
-                      </div> 
-                      <!-- /.form-group -->
-                  </div>
-
-                  <!-- /.col -->
-                </form>
-                </div>
-                <!-- /.row -->
+          <div class="box">
+              <div class="box-header">
+                <h3 class="box-title">
+                  <!-- <a href="<?php echo base_url();?>MasterObat/add" role="button" class="btn btn-success btn-fill pull-left" ><i class='fa fa-sliders'></i> &nbsp;FILTER DATA</a> -->
+                  <button type="button" name="add" id="add" class="btn btn-success"; data-toggle="modal" data-target="#modal_filter"><i class='fa fa-sliders'></i> &nbsp;FILTER DATA</button>
+                </h3>
               </div>
               <!-- /.box-header -->
               <div class="box-body">
@@ -126,7 +88,7 @@
               <div class="modal-dialog modal-dialog-centered">
                   <div class="modal-content">
                       <div class="modal-header">
-                          <h4>Hapus Data Transaksi</h4>
+                          <h4>Hapus Data Obat</h4>
                       </div>
                       <div class="modal-body">
                           Apakah anda yakin akan menghapus data ini?
@@ -139,6 +101,84 @@
               </div>
             </div>
           <!-- /.modal delete -->
+          <div class="modal fade" id="modal_filter" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+            <div class="modal-dialog modal-dialog-centered modal-lg" role="document">
+                <div class="modal-content">
+                  <form action="" method="post">
+                    <div class="modal-header">
+                        <h5 class="modal-title" id="exampleModalLongTitle">Set Filter Data</h5>
+                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                            <span aria-hidden="true">&times;</span>
+                        </button>
+                    </div>
+                    <div class="modal-body" style="margin:auto; width:80%">
+                        <!-- @include('t_log_problem_analisys._form') -->
+                        <div class="row" style="margin: 20px;">
+                            <div class="form-group">
+                                <label class="col-md-4 control-label">Mulai Tanggal</label>
+                                <div class="col-md-7">
+                                <input type="date" class="form-control" name="min" value="<?php echo $min ?>" required>
+                                </div>
+                            </div>  
+                            <!-- end form-group -->
+                        </div> 
+                        <!-- end row -->
+                        <div class="row" style="margin: 20px;">
+                            <div class="form-group">
+                                <label class="col-md-4 control-label">Sampai Tanggal</label>
+                                <div class="col-md-7">
+                                  <input type="date" class="form-control" name="max" placeholder="Nama Obat" value="<?php echo $max ?>" required>
+                                </div>
+                            </div>  
+                            <!-- end form-group -->
+                        </div> 
+                        <!-- end row -->
+                        <div class="row" style="margin: 20px;">
+                            <div class="form-group">
+                                <label class="col-md-4 control-label">Nama Obat</label>
+                                <div class="col-md-7">
+                                <select class="form-control select2" style="width: 100%;" id="obat_id" name="obat" required>
+                                    <option value="" disabled selected>Pilih Obat</option>
+                                    <?php foreach($obat as $key => $value){ ?> 
+                                        <!-- foreach -->
+                                    <option value="<?php echo $value['id'];?>"><?php echo $value['kode_obat']." - ". $value['nama_obat'];?></option>
+                                    <?php } ?>
+                                        <!-- end foreach -->
+                                    </select>
+                                </div>
+                            </div>  
+                            <!-- end form-group -->
+                        </div> 
+                        <!-- end row -->
+
+                        <div class="row" style="margin: 20px;">
+                            <div class="form-group">
+                                <label class="col-md-4 control-label">Supplier</label>
+                                <div class="col-md-7">
+                                <select class="form-control select2" style="width: 100%;" id="supplier_id" name="supplier" required>
+                                    <option value="" disabled selected>Pilih Supplier</option>
+                                    <?php foreach($dosis as $key => $value){ ?> 
+                                        <!-- foreach -->
+                                    <option value="<?php echo $value['id'];?>"><?php echo $value['kode_supplier']." - ". $value['nama_supplier'];?></option>
+                                    <?php } ?>
+                                        <!-- end foreach -->
+                                    </select>
+                                </div>
+                            </div>  
+                            <!-- end form-group -->
+                        </div>
+                        <!-- end row -->
+
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-dismiss="modal">Tutup</button>
+                        <button type="submit" class="btn btn-primary" id="simpan">Cari</button>
+                    </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+                <!-- End Modal -->
         </div>
         <!-- /.col -->
       </div>
